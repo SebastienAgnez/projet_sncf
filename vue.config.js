44b01327889b674f28bd67677ff4667db61bd97f
SebastienAgnez <sebastien.agnez@orange.fr>
2022-01-06 16:07:34 +0100

    ajout du publicpath

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,10 @@
+const publicPath = process.env.NODE_ENV === 'production' ? '/projet_sncf/' : '/'
+
 module.exports = {
+  publicPath: publicPath,
   pluginOptions: {
     vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+    }
   }
 }
